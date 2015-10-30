cf52b7678f40ed6a10710f5e71d2c564f0c74309
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-10-30 20:10:26 +0100

    melhorias

--- crm/vtigercrm-installer.sh
+++ crm/vtigercrm-installer.sh
@@ -1,2 +1,8 @@
 echo "Welcome to VtigerCRM Installer"
 
+wget http://downloads.sourceforge.net/project/vtigercrm/vtiger%20CRM%206.4.0/Core%20Product/vtigercrm6.4.0.tar.gz?r=&ts=1446228893&use_mirror=netcologne
+
+tar -vzxf vtigercrm6.4.0.tar.gz
+
+
+
