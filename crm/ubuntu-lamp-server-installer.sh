f554457d12a498fea6e6a118febd5d87a9eb74e9
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-10-30 18:40:04 +0100

    Updates

---
+++ crm/ubuntu-lamp-server-installer.sh
@@ -0,0 +1,21 @@
+echo "Welcome LAMP Server Installer"
+
+sudo apt-get install -y lamp-server^ phpmyadmin
+
+# Install Apache
+#sudo apt-get -y install apache2
+
+#sudo a2dismod mpm_event
+#sudo a2enmod mpm_prefork
+
+
+#echo "Install MySQL Server"
+#sudo apt-get -y install mysql-server
+
+#echo "Install PHP5"
+#sudo apt-get -y install php5 libapache2-mod-php5 php-pear
+
+#echo "Install PhpMyAdmin"
+#sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin
+
+
